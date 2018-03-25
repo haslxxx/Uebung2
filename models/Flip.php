c1958ea35f2c56f737ea417faa46ad08361c40eb
Willie Wonka <williwonka@noreply.users.github.com>
2018-03-25 14:27:17 +0200

    Erste loop methode (flip) sowie json eingangsdaten einlesen klappt

--- models/Flip.php
+++ models/Flip.php
@@ -15,11 +15,23 @@ class Flip {
     
     private $flippedArray;
     // TODO (1) im construktor  bereits ergebnisarray herstellen, weil vielmehr tut die klasse ja nicht
-    public function __construct() {
-        ;
+    public function __construct($arrayToConvert) {
+        $this->flippedArray = $this->flip($arrayToConvert);
     }
+    
+    private function flip ($toBeFlipped) {
+        $size = count($toBeFlipped);
+        for ($xi=$size;$xi>0;$xi--){
+            //$resultIndex = 0;
+            $resultArray[] = $toBeFlipped[$xi-1]; // anhängen hinten braucht keinen index
+            //$resultIndex++;
+        }
+//        echo implode(", ", $resultArray);
+        return $resultArray;
+    }
+
     //Ergebnis array zurückliefern  .. ein getter
     public function getFlipped(){
-        return $flippedArray;
+        return $this->flippedArray;
     }
 }
